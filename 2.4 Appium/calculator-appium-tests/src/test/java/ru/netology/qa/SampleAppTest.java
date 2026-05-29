90147235ae579a18f11512ec7bc45a5254d3f95c
Grigory-Karpov <gregkarpov64@gmail.com>
2026-05-30 02:13:34 +0400

    Добавлен паттерн PageObjects.

---
+++ 2.4 Appium/calculator-appium-tests/src/test/java/ru/netology/qa/SampleAppTest.java
@@ -0,0 +1,61 @@
+package ru.netology.qa;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SampleAppTest {
+    private AppiumDriver driver;
+    private SampleAppPage page; // Объявляем переменную для страницы
+
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("appium:deviceName", "Some name");
+        desiredCapabilities.setCapability("appium:appPackage", "ru.netology.testing.uiautomator");
+        desiredCapabilities.setCapability("appium:appActivity", "ru.netology.testing.uiautomator.MainActivity");
+        desiredCapabilities.setCapability("appium:automationName", "uiautomator2");
+        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723");
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+
+        // Инициализируем страницу перед каждым тестом
+        page = new SampleAppPage(driver);
+    }
+
+    @Test
+    public void testEmptyStringDoesNotChangeText() {
+        String originalText = page.getTextToBeChanged();
+        page.changeText("   ");
+        Assertions.assertEquals(originalText, page.getTextToBeChanged());
+    }
+
+    @Test
+    public void testOpenTextInNewActivity() throws InterruptedException {
+        String textToType = "Appium Test Netology";
+        page.fillInput(textToType);
+        page.openNewActivity();
+
+        Thread.sleep(3000);
+
+        Assertions.assertEquals(textToType, page.getTextInNewActivity());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
