323874b500ea6c6e0c976cfe906b6fe42dc3964b
Grigory Karpov <gregkarpov64@gmail.com>
2026-05-30 11:53:13 +0400

    Update SampleAppTest.java

--- 2.4 Appium/calculator-appium-tests/src/test/java/ru/netology/qa/SampleAppTest.java
+++ 2.4 Appium/calculator-appium-tests/src/test/java/ru/netology/qa/SampleAppTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 public class SampleAppTest {
     private AppiumDriver driver;
-    private SampleAppPage page; // Объявляем переменную для страницы
+    private SampleAppPage page; 
 
     @BeforeEach
     public void setUp() throws MalformedURLException {
@@ -29,8 +31,10 @@ public class SampleAppTest {
 
         URL remoteUrl = new URL("http://127.0.0.1:4723");
         driver = new AndroidDriver(remoteUrl, desiredCapabilities);
-
-        // Инициализируем страницу перед каждым тестом
+        
+        // Включаем неявное ожидание (максимум 10 секунд на поиск любого элемента)
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        
         page = new SampleAppPage(driver);
     }
 
@@ -42,13 +46,13 @@ public class SampleAppTest {
     }
 
     @Test
-    public void testOpenTextInNewActivity() throws InterruptedException {
+    public void testOpenTextInNewActivity() {
         String textToType = "Appium Test Netology";
         page.fillInput(textToType);
         page.openNewActivity();
-
-        Thread.sleep(3000);
-
+        
+        // Thread.sleep удален, драйвер сам подождет появления новой Activity благодаря implicitlyWait
+        
         Assertions.assertEquals(textToType, page.getTextInNewActivity());
     }
 
